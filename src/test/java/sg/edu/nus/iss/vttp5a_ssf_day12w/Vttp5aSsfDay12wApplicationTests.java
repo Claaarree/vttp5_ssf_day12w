abe2642e15b9501282f9bbe878db567d1120af1d
Claaarree <clare.fh@hotmail.com>
2024-11-16 17:18:16 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_ssf_day12w/Vttp5aSsfDay12wApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day12w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay12wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
